a1439c6edf097672f70b622766547e4e49c9d651
Dmitrijs Dums <dd636856@gmail.com>
2026-03-01 22:08:33 +0000

    implement MongoDB connection and update package dependencies

---
+++ backend/server.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI as string, {
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+})
